cedfe10530c85f4df8e639fc9fcb373e6534a386
mridular <mridula.r@ictkerala.org>
2023-03-12 21:35:47 +0530

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(fileUpload({
     tempFileDir : '/tmp/'
 }));
 
-app.post('/file', function(req, res) {
+app.post('/api/file', function(req, res) {
    
   let sampleFile;
   let uploadPath;
