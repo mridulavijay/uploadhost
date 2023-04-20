eae675842a5e7ed0797bc9ebd37ba7895cd64690
mridular <mridula.r@ictkerala.org>
2023-04-20 21:40:26 +0530

    change2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 // default options
-app.use(express.static(path.join(__dirname,'/tmp')));  
+app.use(express.static(path.join(__dirname,'/build')));  
 app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
